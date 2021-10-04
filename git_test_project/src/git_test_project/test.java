4b078bd169713615c67fcd21d3031b98d237aeb3
USER <USER@DESKTOP-MCLCQP1>
2021-10-04 15:53:38 +0900

    자바프로젝트 첫번째 커밋!!

---
+++ git_test_project/src/git_test_project/test.java
@@ -0,0 +1,7 @@
+package git_test_project;
+
+public class test {
+//여기서 소스를 수정하면 git의 워크트리에 있는 파일이 수정되는 것
+//워크트리    -> 인덱스         -> 로컬저장소    -> 원격저장소
+//       "add index"     "commit"       "push"
+}
